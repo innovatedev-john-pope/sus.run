35d002044008bfa8b7649b643fd4c4089559aacf
John Pope <88814278+innovatedev-john-pope@users.noreply.github.com>
2023-06-13 22:10:48 -0600

    add oauth kv keys

--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,9 @@
+import { getSessionId } from "https://deno.land/x/deno_kv_oauth@v0.2.0-beta/mod.ts";
 import settings from "../settings.ts";
 
 export const kv = await Deno.openKv(settings.database.path);
+
+getSessionId
 
 export const KEY_PREFIX = {
   urls: "urls",
@@ -8,4 +11,8 @@ export const KEY_PREFIX = {
   urlDestinations: "url-destinations",
   session: "session",
   users: "users",
+
+  // from deno_kv_oauth
+  oauthSessions: "oauth_sessions",
+  storedTokensBySession: "stored_tokens_by_session",
 }
