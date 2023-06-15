214e6c20ef743a57c8e7ffe68dd694b9bacfdb96
John Pope <88814278+innovatedev-john-pope@users.noreply.github.com>
2023-06-15 13:40:39 -0600

    update shortcode island

--- islands/ShortCode.tsx
+++ islands/ShortCode.tsx
@@ -3,15 +3,15 @@ import { useSignal } from "@preact/signals";
 export default function ShortCode({link}: {link: string}) {
   const didCopy = useSignal(false);
 
-  return <div class="flex flex-col items-center mb-8">
+  return <div class="flex flex-col items-center mb-8 cursor-pointer" onClick={() =>{
+    navigator.clipboard.writeText(link);
+    didCopy.value = true;
+  }}>
     <div class={`border p-4 rounded-lg relative ${didCopy.value ? 'bg-green-200' : 'bg-gray-200'}`}>
-      <div class="text-4xl pb-2">
+      <div class="text-2xl pb-2">
         {link}
       </div>
-      <div class="text-xs absolute bottom-0 right-0 p-1" onClick={() =>{
-        navigator.clipboard.writeText(link);
-        didCopy.value = true;
-      }}>
+      <div class="text-xs absolute bottom-0 right-0 p-1">
         {didCopy.value ? 'Link copied' : 'Copy link'}
       </div>
     </div>
